949632788840562124e29e228560fc2af9ff0e0d
ImmG <102626791+gripping-immensity@users.noreply.github.com>
2022-08-05 15:41:11 +0300

    minor fix

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,9 +1,9 @@
 #include "lib.h"
-
 #include <iostream>
 
-int main (int, char **) {
-    std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;
+int main (int argc, char **) {
+    std::cout << "build " << version() << "\n";
+    std::cout << "Hello, World!" << "\n";
+
     return 0;
 }
--- hw1/test_version.cpp
+++ hw1/test_version.cpp
@@ -1,17 +1,7 @@
 #include <gtest/gtest.h>
 #include "lib.h"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
-}
-
-#include <gtest/gtest.h>
-
 TEST(HelloTest, ValidVersion) {
-  EXPECT_GT(version(), 0);
+    EXPECT_GT(version(), 0);
 }
 
