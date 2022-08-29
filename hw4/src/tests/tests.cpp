7cf6a58c19cb0f10782f4cc85d947da8b89e305e
ImmG <102626791+gripping-immensity@users.noreply.github.com>
2022-08-29 15:56:58 +0300

    hw4 init

---
+++ hw4/src/tests/tests.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "util.h"
+
+TEST(IpReprTests, String) {
+    EXPECT_EQ("hello", GetStringRepr("hello"));
+    EXPECT_EQ("a", GetStringRepr(std::string{"a"}));
+    std::string x = "xy";
+    std::string y = GetStringRepr(x);
+    EXPECT_EQ(x, y);
+    std::string r = "lll";
+    EXPECT_EQ(r, GetStringRepr(r));
+    EXPECT_EQ((void *)(r.c_str()), (void *) (GetStringRepr(r).c_str()));
+    EXPECT_EQ("", GetStringRepr(""));
+}
+
+TEST(IpReprTests, Int) {
+    EXPECT_EQ("255", GetStringRepr(int8_t{-1}));
+    EXPECT_EQ("0.0.0.0", GetStringRepr(int32_t{0}));
+    EXPECT_EQ("127.0.0.1", GetStringRepr(int32_t{2130706433}));
+}
+
+TEST(IpReprTests, Tuple) {
+    EXPECT_EQ("0", GetStringRepr(std::tuple<int>{0}));
+    EXPECT_EQ("1.1.1.1",
+              GetStringRepr(std::tuple<std::string, std::string, std::string, std::string>{"1", "1", "1", "1"}));
+}
+
+TEST(IpReprTests, Vector) {
+    EXPECT_EQ("5.5.5.5", GetStringRepr(std::vector<char>{'5', '5', '5', '5'}));
+    EXPECT_EQ("100.99.88.77", GetStringRepr(std::vector<int>{100, 99, 88, 77}));
+}
+
+TEST(IpReprTests, List) {
+    EXPECT_EQ("50.1", GetStringRepr(std::list<long long>{50, 1}));
+}
---
+++ hw4/src/util.h
@@ -0,0 +1,122 @@
+#pragma once
+#include <cstdlib>
+#include <sstream>
+#include <list>
+#include <vector>
+#include <tuple>
+
+
+template <typename T>
+struct is_vector : std::false_type {};
+
+template <typename T, typename U>
+struct is_vector<std::vector<T, U>> : std::true_type {};
+
+template <typename T>
+inline constexpr bool is_vector_v = is_vector<T>::value;
+
+template <typename T>
+struct is_list : std::false_type {};
+
+template <typename T, typename U>
+struct is_list<std::list<T, U>> : std::true_type {};
+
+template <typename T>
+inline constexpr bool is_list_v = is_list<T>::value;
+
+template <typename T>
+struct is_tuple : std::false_type {};
+
+template <typename... Args>
+struct is_tuple<std::tuple<Args...>> : std::true_type {};
+
+template <typename T>
+inline constexpr bool is_tuple_v = is_tuple<T>::value;
+
+template <typename T>
+inline constexpr bool is_cstr_v = std::is_same_v<const char*, T> || std::is_same_v<char*, T>;
+
+template <typename T>
+struct is_string_literal : std::false_type {};
+
+template <size_t N>
+struct is_string_literal<const char (&) [N]> : std::true_type {};
+
+template <typename T>
+inline constexpr bool is_string_literal_v = is_string_literal<T>::value;
+
+template <typename T, typename S, size_t N>
+struct IntegralReprImpl {
+    static void Get(T x, S& ss) {
+        int b = (x >> 8 * N) & (0xff);
+        ss << b << ".";
+        IntegralReprImpl<T, S, N - 1>::Get(x, ss);
+    }
+};
+
+template <typename T, typename S>
+struct IntegralReprImpl<T, S, 0> {
+    static void Get(T x, S& ss) {
+        int b = x & (0xff);
+        ss << b;
+    }
+};
+
+template <typename T, typename std::enable_if_t<std::is_integral_v<T>>* = nullptr>
+std::string GetStringRepr(T x) {
+    std::stringstream ss;
+    IntegralReprImpl<T, decltype(ss), sizeof(x) - 1>::Get(x, ss);
+    return ss.str();
+}
+
+template <typename T, typename std::enable_if_t<std::is_same_v<std::string, std::decay_t<T>>>* = nullptr>
+T&& GetStringRepr(T&& x) {
+    return std::forward<T>(x);
+}
+
+template <typename T, typename std::enable_if_t<is_cstr_v<T> || is_string_literal_v<T>>* = nullptr>
+std::string GetStringRepr(T x) {
+    return {x};
+}
+
+template <typename T, typename = std::enable_if_t<is_vector_v<T> || is_list_v<T>>>
+std::string GetStringRepr(const T& cont) {
+    std::stringstream ss;
+
+    auto it = cont.begin();
+    for (size_t i = 0; i + 1 < cont.size(); ++i, ++it) {
+        ss << *it << ".";
+    }
+    ss << *it;
+
+    return ss.str();
+}
+
+template <typename T, typename E, typename S, size_t N>
+struct TupleReprImpl {
+    static void Get(const T& t, S& ss) {
+        constexpr size_t i = std::tuple_size<T>::value - N - 1;
+        static_assert(std::is_same_v<E, std::tuple_element_t<i, T>>,
+                "The elements must be the same type.");
+        ss << std::get<i>(t) << ".";
+        TupleReprImpl<T, E, S, N - 1>::Get(t, ss);
+    }
+};
+
+template <typename T, typename E, typename S>
+struct TupleReprImpl<T, E, S, 0> {
+    static void Get(const T& t, S& ss) {
+        constexpr size_t i = std::tuple_size<T>::value - 1;
+        static_assert(std::is_same_v<E, std::tuple_element_t<i, T>>,
+                      "The elements must be the same type.");
+        ss << std::get<i>(t);
+    }
+};
+
+template <typename T, typename std::enable_if<is_tuple_v<T>>::type* = nullptr>
+std::string GetStringRepr(const T& t) {
+    std::stringstream ss;
+    TupleReprImpl<T, std::tuple_element_t<0, T>, std::stringstream, std::tuple_size_v<T> - 1>::Get(t, ss);
+    return ss.str();
+}
+
